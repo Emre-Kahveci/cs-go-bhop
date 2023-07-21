edf170133d5c68acb62ff73d34cda419f53a396d
Emre-Kahveci <aominemre@gmail.com>
2023-07-21 18:46:23 +0300

    client find improvement

--- cs-go-bhop.py
+++ cs-go-bhop.py
@@ -1,6 +1,7 @@
 import pymem
 import win32api
 import time
+from pymem.process import *
 
 # offsets
 LOCAL_PLAYER = 14592396
@@ -10,32 +11,29 @@ FLAGS = 260
 
 def bhop() -> None:
     pm = pymem.Pymem('csgo.exe')
-    print(pm)
-    # get module address
-    for module in list(pm.list_modules()):
-        if module.name == 'client.dll':
-            print("modul bulundu")
-            client = module.lpBaseOfDll
 
+    client = module_from_name(pm.process_handle, "client.dll").lpBaseOfDll
 
     # hack loop
     while True:
         time.sleep(0.01)
 
+        # if you press/hold the "space" key
         if not win32api.GetAsyncKeyState(0x20):
             continue
-
         
         local_player = int(pm.read_uint(client + LOCAL_PLAYER))
+        # if player is available
         if not local_player:
             continue
         
-        # is alive
+        # if alive
         if not pm.read_uint(local_player + HEALTH):
             continue
 
-        # on ground
+        # if on the ground
         if pm.read_uint(local_player + FLAGS) & 1 << 0:
+            # jump
             pm.write_uint(client + FORCE_JUMP, 6)
             time.sleep(.01)
             pm.write_uint(client + FORCE_JUMP, 4)
